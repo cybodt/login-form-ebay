515cfbe97db05e9a7882277e23f5c03a053b5f5b
cybodt <cybodt@live.com>
2023-08-26 19:36:36 +0300

    share code on github

--- src/SignUpPersonal.jsx
+++ src/SignUpPersonal.jsx
@@ -1,46 +1,55 @@
 import React from 'react';
 import './SignUpPersonal.css';
+import { Link } from 'react-router-dom';
 
 function SignUpPersonal() {
   return (
     <div className='sign-up-personal__container'>
       <form className='sign-up-personal__form'>
         <div className='sign-up-personal__first-and-last-name'>
-          <label htmlFor='first-name'>First name</label>
+          <div className='sign-up-personal__first-name'>
+            <label htmlFor='first-name'>First name</label>
+            <input
+              type='text'
+              id='first-name'
+              className='sign-up-personal__input-first-name'
+            />
+          </div>
+          <div className='sign-up-personal__last-name'>
+            <label htmlFor='last-name'>Last name</label>
+            <input
+              type='text'
+              id='last-name'
+              className='sign-up-personal__input-last-name'
+            />
+          </div>
+        </div>
+        <div className='sign-up-personal__email'>
+          <label htmlFor='sign-up-email'>Email</label>
           <input
             type='text'
-            id='first-name'
-            className='sign-up-personal__input-first-name'
-          />
-          <label htmlFor='last-name'>Last name</label>
-          <input
-            type='text'
-            id='last-name'
-            className='sign-up-personal__input-last-name'
+            id='sign-up-email'
+            className='sign-up-personal__input-email'
           />
         </div>
-        <label htmlFor='sign-up-email'>Email</label>
-        <input
-          type='text'
-          id='sign-up-email'
-          className='sign-up-personal__input-email'
-        />
-        <label htmlFor='sign-up-password'>Password</label>
-        <input
-          type='password'
-          id='sign-up-password'
-          className='sign-up-personal__input-password'
-        />
+        <div className='sign-up-personal__input-password'>
+          <label htmlFor='sign-up-password'>Password</label>
+          <input
+            type='password'
+            id='sign-up-password'
+            className='sign-up-personal__input-password'
+          />
+        </div>
         <div className='sign-up-personal__agreement-and-notice'>
-
-          tova e samo test
-          tova e test 2
+          <p>
+            By <b>Creating an account</b>, you agree to our <Link to='/useragreement'>User Agreement</Link> and acknowledge reading our <Link to='/userprivacynotice'>User Privacy Notice </Link>.
+          </p>
         </div>
         <button type='submit'>Create account</button>
       </form>
       <div className='sign-up-personal__vertical-delimiter'></div>
       <div className='sign-up-personal__social-login'></div>
-    </div>
+    </div >
   );
 }
 
